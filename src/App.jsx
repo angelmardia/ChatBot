4b82bcf14cb5d59e33ac587c9c6eed25df942430
angel <angelmardia.xt@outlook.com>
2025-01-13 12:28:29 +0530

    ENV fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CustomChat from "./components/CustomChat";
 import { SessionProvider, useSession } from "./SessionContext";
 import "./App.css";
 
-const BACKEND_URL = 'http://localhost:5000';// Replace with your backend URL
+const BACKEND_URL = import.meta.env.VITE_PUBLIC_BACKEND_URL;
 
 function AppContent() {
   const { setSessionId } = useSession();
